f0443d52c0418c63bdded04f3fd8eafa415b3d5e
dimitrikarpov <karpov.dimon@gmail.com>
2023-09-13 10:11:42 +0500

    deploy to github

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/dummy-redux-cart",
   plugins: [react()],
   resolve: {
     alias: {
